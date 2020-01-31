40fdf4001019921d7499f09b45aa51399aaf5f24
Juan Luis Polo <jlpolog  git config --global user.name Your>
2020-01-30 18:44:06 -0600

    nuevo codigo

---
+++ hola.c
@@ -0,0 +1,20 @@
+//
+//  storage_mgr.c
+//  ASS1_StorageManager
+//
+//  Created by Juan Luis Polo and Eduardo Moreno on Spring 2020.
+//  Copyright © 2020 Juan Luis Polo and Eduardo Moreno. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//File Pointer
+FILE *fp;
+
+extern void initStorageManager (void){
+    printf("Store Manager initialized");
+    free(fp);
+}
